74c5effaf7a393881b3f2983ed1f3adb970575e4
Darkkyelfo <raulpedrouag@gmail.com>
2015-08-21 13:33:26 -0300

    GUI do Campo
    GUI do campo Gerada após a conversão do arquivo .ui para py.

---
+++ Imagens/Campo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Campo2.ui'
+#
+# Created: Thu Aug 20 18:14:12 2015
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
